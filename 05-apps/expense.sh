d84e71516e44db9c5c87295f28c23a70b38fbf71
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-05-30 15:47:55 +0530

    committing changes in expense file

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -4,5 +4,5 @@ dnf install ansible -y  #by default, user data will have sudo access
 cd /tmp
 git clone https://github.com/NagamaniPalemkota/expense-ansible-roles.git
 cd expense-ansible-roles
-ansible-playbook main.yaml -e component=backend -e password=ExpenseApp1
+ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
